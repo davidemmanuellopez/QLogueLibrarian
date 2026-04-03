2ffed17e672f1da9b48000c20711b92ef94a52b0
david <david.emmanuel.lopez@gmail.com>
2026-04-03 20:11:51 -0300

    qml migration

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,21 @@
 #include <QApplication>
-#include "view/MainWindow.h"
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include "controller/AppController.h"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     app.setApplicationName(QStringLiteral("QLogueLibrarian"));
     app.setOrganizationName(QStringLiteral("qlogue"));
 
-    qlogue::MainWindow w;
-    w.show();
+    qlogue::AppController controller;
+
+    QQmlApplicationEngine engine;
+    engine.rootContext()->setContextProperty(QStringLiteral("App"), &controller);
+
+    engine.load(QUrl(QStringLiteral("qrc:/qml/Main.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
     return app.exec();
 }
